b92092148d12172bb8c935f5fb35d92ccf3c014a
Fabian Trautsch <fabian.trautsch@informatik.uni-goettingen.de>
2016-11-10 15:19:53 +0100

    added setup.py

---
+++ setup.py
@@ -0,0 +1,28 @@
+import os
+import sys
+
+from setuptools import setup, find_packages
+
+setup(
+    name='issueSHARK',
+    version='0.1',
+    description='Collect data from issue tracking systems',
+    install_requires=['requests', 'mongoengine', 'pymongo', 'python-dateutil'],
+    author='Fabian Trautsch',
+    author_email='ftrautsch@googlemail.com',
+    url='https://github.com/smartshark/issueSHARK',
+    test_suite='tests',
+    packages=find_packages(),
+    zip_safe=False,
+    include_package_data=True,
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Development Status :: 4 - Beta",
+        "Environment :: Console",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: Apache2.0 License",
+        "Operating System :: POSIX :: Linux",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+    ],
+)
+
