a902b08fe4f77cc275e89921cc4214d58a4c8054
Fabian Trautsch <fabian.trautsch@informatik.uni-goettingen.de>
2016-11-28 11:11:35 +0100

    changes for new database models

--- plugin_packaging/execute.sh
+++ plugin_packaging/execute.sh
@@ -1,11 +1,24 @@
 #!/bin/sh
 PLUGIN_PATH=$1
 
-COMMAND="python3.5 $1/main.py --db-user $2 --db-password $3 --db-database $4 --db-hostname $5 --db-port $6 --db-authentication $7 --url $8 --issueurl $9 --backend ${10}"
+COMMAND="python3.5 $1/main.py --db-database $4 --db-hostname $5 --db-port $6 --project-name $8 --issueurl $9 --backend ${10}"
 
 if [ ! -z ${11} ] && [ ${11} != "None" ]; then
 	COMMAND="$COMMAND --token ${11}"
 fi
+
+if [ ! -z ${2} ] && [ ${2} != "None" ]; then
+	COMMAND="$COMMAND --db-user ${2}"
+fi
+
+if [ ! -z ${3} ] && [ ${3} != "None" ]; then
+	COMMAND="$COMMAND --db-password ${3}"
+fi
+
+if [ ! -z ${7} ] && [ ${7} != "None" ]; then
+	COMMAND="$COMMAND --db-authentication ${7}"
+fi
+
 
 if [ ! -z ${12} ] && [ ${12} != "None" ]; then
 	if [ ! -z ${13} ] && [ ${13} != "None" ]; then
@@ -29,6 +42,14 @@ fi
 
 if [ ! -z ${16} ] && [ ${16} != "None" ]; then
     COMMAND="$COMMAND --debug ${16}"
+fi
+
+if [ ! -z ${17} ] && [ ${17} != "None" ]; then
+    COMMAND="$COMMAND --issue-user ${17}"
+fi
+
+if [ ! -z ${18} ] && [ ${18} != "None" ]; then
+    COMMAND="$COMMAND --issue-password ${18}"
 fi
 
 $COMMAND
