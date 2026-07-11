f7b94703e77df60a78aadfaab617cc85ef7cbafc
To Que Phuong Tran <tran16@ads.uni-passau.de>
2026-07-11 14:45:07 +0200

    Correct pycoshark tag to 2.0.0 and resolve redundant installation

--- plugin_packaging/install.sh
+++ plugin_packaging/install.sh
@@ -3,5 +3,4 @@ PLUGIN_PATH=$1
 cd $PLUGIN_PATH
 
 # Install issueSHARK
-python3 -m pip install --user --no-cache-dir --upgrade --upgrade-strategy only-if-needed "pycoshark @ git+https://github.com/smartshark/pycoSHARK.git@2.0.0"
 python3 $PLUGIN_PATH/setup.py install --user
--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ setup(
     install_requires=['mongoengine>=0.23.0', 'pymongo', 'requests>=2.10.0', 'oauthlib>=3.0.0',
                       'cryptography>=1.3.4', 'python-dateutil', 'validate_email',
                       'jira==2.0.0', 
-                      'pycoshark @ git+https://github.com/smartshark/pycoSHARK.git@v2.0.0', 
+                      'pycoshark @ git+https://github.com/smartshark/pycoSHARK.git@2.0.0', 
                       'mock', 
                       'deepdiff<=5.5.0'],
     url='https://github.com/smartshark/issueSHARK',
